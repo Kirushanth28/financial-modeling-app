171c400971874919a7bc8569d8dc55b780cc93b9
Kirushanth <skirush28@gmail.com>
2023-10-30 00:03:48 +1100

    login and register

--- resources/js/pages/login.jsx
+++ resources/js/pages/login.jsx
@@ -1,56 +1,119 @@
 import React, { useState } from 'react';
 import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink } from '@inertiajs/inertia-react';
+import CompanyLogo from "../../../public/images/company_logo.png";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    email_or_name: '',
-    password: '',
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    Inertia.post('/login', formData, {
-        onSuccess: () => {
-          // Redirect to the desired page after successful login
-          // Inertia.visit('/dashboard');
-        },
-        onError:() => {
-        }
+    const [formData, setFormData] = useState({
+        email_or_name: '',
+        password: '',
     });
-  };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <div>
-      <h1 className="text-blue-600 hover:text-red-600">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>User name or Email</label>
-        <input
-          name="email_or_name"
-          value={formData.email_or_name}
-          onChange={handleInputChange}
-        />
-        <p></p>
-        <label>Password</label>
-        <input
-          name="password"
-          type="password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        <p></p>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+        Inertia.post('/login', formData, {
+            onSuccess: () => {
+                // Redirect to the desired page after successful login
+                // Inertia.visit('/dashboard');
+            },
+            onError: () => {
+            }
+        });
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-24 w-auto"
+                    src={CompanyLogo}
+                    alt="Company Logo"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="sign-in-form">
+            <form className="space-y-6" action="#" onSubmit={handleSubmit}>
+                <div>
+                    <label
+                        htmlFor="email"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                        Email address
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            name="email_or_name"
+                            value={formData.email_or_name}
+                            onChange={handleInputChange}
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 pl-3 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center justify-between">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Password
+                        </label>
+                        <div className="text-sm">
+                            <a
+                                href="#"
+                                className="font-semibold text-indigo-600 hover:text-indigo-500"
+                            >
+                                Forgot password?
+                            </a>
+                        </div>
+                    </div>
+                    <div className="mt-2">
+                        <input
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleInputChange}
+                            autoComplete="current-password"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-3 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-primary py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-900"
+                    >
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        </div>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Not a member yet?{" "}
+                    <InertiaLink href="/register" className="font-semibold leading-6 text-primary">Register</InertiaLink>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
