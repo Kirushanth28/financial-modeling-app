1ffc0a569338654e4daeaaeefee6b280ba935611
Kirushanth <skirush28@gmail.com>
2023-10-31 22:24:15 +1100

    Company profile and quote UI changes

--- resources/js/Pages/companyprofile.jsx
+++ resources/js/Pages/companyprofile.jsx
@@ -10,33 +10,49 @@ const CompanyProfile = () => {
     }, []);
 
     return (
-        <div className="mx-auto px-10 w-full pt-10">
-            <div className="flex items-center justify-between mb-6">
-                <h1 className="text-2xl text-black">Company Profile</h1>
-                <div><InertiaLink
-            href="/home"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Back to search
-          </InertiaLink>
-          </div>
+        <div className="mx-auto px-10 md:w-1/2 w-full pt-10">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+                <div class="flex justify-start text-2xl text-black">Company Profile</div>
+                <div class="flex justify-end">
+                    <InertiaLink
+                        href="/home"
+                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Back to search
+                    </InertiaLink>
+                </div>
             </div>
-            <table className="table-auto shadow">
-                <thead>
-                    <tr tabindex="1" class="bg-gray-300 h-10 border-none">
-                        <th>Attribute</th>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.entries(profile[0]).map(([key, value]) => (
-                        <tr className="max-w-xs break-words" key={key}>
-                            <td className="border px-4 py-2 overflow-hidden">{key}</td>
-                            <td className="border px-4 py-2 overflow-hidden">{value}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div class="flex flex-col overflow-x-auto mt-3">
+                <div class="sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div class="overflow-x-auto">
+                            <table class="min-w-full text-left text-sm font-light">
+                                <thead class="border-b font-medium dark:border-neutral-500">
+                                    <tr>
+                                    <th scope="col" class="px-6 py-4">Attribute</th>
+                                    <th scope="col" class="px-6 py-4">Value</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                    profile[0]?
+                                    Object.entries(profile[0]).map(([key, value]) => (
+                                    <tr class="border-b dark:border-neutral-500 break-words" key={key}>
+                                        <td class="px-6 py-4">{key}</td>
+                                        <td class="px-6 py-4">{value}</td>
+                                    </tr>
+                                    ))
+                                    :
+                                    <tr class="border-b dark:border-neutral-500">
+                                        <td colSpan={2} className="border px-4 py-2 overflow-hidden">No results found</td>
+                                    </tr>
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
